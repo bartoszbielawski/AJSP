25560a63a50a4e5fc0f24d3cb6398fd184b80edf
Bartosz Bielawski <bartosz.bielawski@gmail.com>
2017-06-13 00:13:16 +0200

    Cleanup in examples and extra

---
+++ examples/MapCollector/MapCollector.ino
@@ -0,0 +1,29 @@
+#include <MapCollector.hpp>
+
+void setup() {
+  Serial.begin(57600);
+}
+
+bool predicate(const std::string& path, const std::string& value)
+{
+  String p(path.c_str());
+  return p.startsWith("/root/0/");    //select only elements with path beginning with /root/0
+}
+
+const char data[] = "[{'a': 'A', 'b': 'B'}, 'test']";
+
+void loop() {
+  MapCollector mc(predicate);
+  for (const auto& c: data)
+  {
+    mc.parse(c);
+  }
+
+  for (const auto& p: mc.getValues())
+  {
+    Serial.printf("%s - %s\n", p.first.c_str(), p.second.c_str());
+  }
+  // Prints:
+  // /root/0/a - A
+  // /root/0/b - B
+}
